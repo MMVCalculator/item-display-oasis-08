29e85f33fe6bbe220bb97d8667896749877e4d90
MMVCalculator <pisanupong001@gmail.com>
2025-04-20 01:31:53 +0700

    Update crops
    add tomato

--- src/data/categories/crops.ts
+++ src/data/categories/crops.ts
@@ -1,3 +1,11 @@
 import { Item } from "../../types/Item";
 
-export const crops: Item[] = [];
+export const crops: Item[] = [
+   {
+    name: "Tomato",
+    image:
+      "https://whitepaper-th.morningmoonvillage.com/~gitbook/image?url=https%3A%2F%2F3405632122-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252F-Mi6LXCVY3A30JRGM2nH%252Fuploads%252Fb8MLPPpCLDpCuUYqglLq%252Fcrop_tomato.png%3Falt%3Dmedia%26token%3D67ae2545-c159-44e6-80e9-26009a8a9d7a&width=300&dpr=1&quality=100&sign=67a686c1&sv=1",
+    quantity: 10,
+    address: "0x9Ea7E0435B5E50e1DCBB8Eacd63F0dbD3003BdAA",
+  },
+];
